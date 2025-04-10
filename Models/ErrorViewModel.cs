5673c587434a4d0487a3e654e97fbaf328c66333
Bal-Tzar <ottodemare1@gmail.com>
2025-04-10 12:40:31 +0200

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace merch_store_dhapro.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
